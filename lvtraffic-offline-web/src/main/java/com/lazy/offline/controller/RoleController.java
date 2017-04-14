058a1da798a65cd17facfae92d9758fa1b79953a
xushengb5m <dinglijn@126.com>
2017-04-14 18:50:06 +0800

    吞吞吐吐

--- lvtraffic-offline-web/src/main/java/com/lazy/offline/controller/RoleController.java
+++ lvtraffic-offline-web/src/main/java/com/lazy/offline/controller/RoleController.java
@@ -1,14 +1,9 @@
 package com.lazy.offline.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lazy.offline.dao.mapper.RoleMapper;
 import com.lazy.offline.model.Role;
-import com.lazy.offline.model.User;
 import com.lazy.offline.model.base.BaseQueryDto;
 import com.lazy.offline.model.base.BaseResultDto;
 import com.lazy.offline.model.base.Pagination;
--- lvtraffic-offline-web/src/main/java/com/lazy/offline/controller/RoleResourceController.java
+++ lvtraffic-offline-web/src/main/java/com/lazy/offline/controller/RoleResourceController.java
@@ -1,20 +1,12 @@
 package com.lazy.offline.controller;
 
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.lazy.offline.dao.mapper.RoleMapper;
-import com.lazy.offline.model.Role;
-import com.lazy.offline.model.base.BaseQueryDto;
-import com.lazy.offline.model.base.BaseResultDto;
-import com.lazy.offline.model.base.Pagination;
 
 @Controller
 @RequestMapping("system")
